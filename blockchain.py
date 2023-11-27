9b4064e0e847f1364680a1a12e718ad916974533
Stroe Andre <StroeAndreX.studio@gmail.com>
2023-11-27 02:13:20 +0100

    Python blockchain blueprint class

---
+++ blockchain.py
@@ -0,0 +1,17 @@
+class Blockchain(object):
+    def __init__(self) -> None:
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self) -> None: 
+        pass
+
+    def new_transaction(self) -> None:
+        pass
+
+    @staticmethod
+    def hash(block):
+        pass
+
+    def last_block(self) -> object:
+        pass
