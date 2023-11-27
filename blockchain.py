11b021ab0ab0ba2cc14640d4fa74568adcdfc8bb
Stroe Andre <StroeAndreX.studio@gmail.com>
2023-11-27 02:50:02 +0100

    Add new block in the chain

--- blockchain.py
+++ blockchain.py
@@ -1,17 +1,58 @@
+import hashlib
+import json
+from time import time
+
 class Blockchain(object):
     def __init__(self) -> None:
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self) -> None: 
-        pass
+    def new_block(self, proof, previous_hash=None) -> dict: 
+        """
+        Create a new Block in the Blockchain
+            :param proof: <int> The proof given by the Proof of Work algorithm
+            :param previous_hash: (Optional) <str> Hash of previous Block
+            :return: <dict> New Block
+        """
 
-    def new_transaction(self) -> None:
-        pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = []
+
+        self.chain.append(block)
+           
+        return block
+
+    def new_transaction(self, sender: str, recipient: str, amount: int) -> int:
+
+        """
+         Creates a new transaction to go into the next mined Block
+            :param sender: <str> Address of the Sender
+            :param recipient: <str> Address of the Recipient
+            :param amount: <int> Amount
+            :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+                'sender': sender,
+                'recipient': recipient,
+                'amount': amount})
+        
+        return self.last_block['index'] + 1
 
     @staticmethod
-    def hash(block):
-        pass
+    def hash(block) -> str:
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+        
+        return hashlib.sha256(block_string).hexdigest()
 
-    def last_block(self) -> object:
-        pass
+    def last_block(self) -> dict:
+        return self.chain[-1]
