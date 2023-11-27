abd86d4791c487c38441b920273f31e7fff0114a
Stroe Andre <StroeAndreX.studio@gmail.com>
2023-11-27 03:07:46 +0100

    Implementing basic Proof of Work

--- blockchain.py
+++ blockchain.py
@@ -1,11 +1,16 @@
 import hashlib
 import json
+
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self) -> None:
         self.chain = []
         self.current_transactions = []
+
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None) -> dict: 
         """
@@ -47,6 +52,15 @@ class Blockchain(object):
         
         return self.last_block['index'] + 1
 
+
+    def proof_of_work(self, last_proof) -> int:
+        proof = 0 
+
+        while self.valid_proof(last_proof, proof) is False: 
+            proof += 1
+
+        return proof
+    
     @staticmethod
     def hash(block) -> str:
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
@@ -56,3 +70,9 @@ class Blockchain(object):
 
     def last_block(self) -> dict:
         return self.chain[-1]
+
+    def valid_proof(last_proof, proof) -> bool:
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        
+        return guess_hash[:4] == "0000"
